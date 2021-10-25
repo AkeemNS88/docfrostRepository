8d45a7e4c59a4496c6efd1897d30501e7ef4479f
Akeem <AkeemS11788@gmail.com>
2021-10-25 00:13:46 -0400

    Completed assignment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Behold, things to click on!";
 
 document.querySelector("body").appendChild(h2);
